a27d346c62aca0c7f74a7c067f5d363931cbec76
Ildar Sampilov <ildar.sampilov@yandex.ru>
2022-12-14 23:46:05 +0600

    Initial Commit

---
+++ StateAndDataFlow/StateAndDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowApp.swift
+//  StateAndDataFlow
+//
+//  Created by Цырендылыкова Эржена on 14.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
